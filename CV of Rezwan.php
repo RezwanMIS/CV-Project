94c31ce75d3e2d060e205d2c742959066f2879a7
Rezwan Sheikh <139754574+RezwanMIS@users.noreply.github.com>
2024-03-18 11:52:09 +0600

    Add HTML and CSS file for the CV

---
+++ CV of Rezwan.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV OF REZWAN</title>
+    <link rel="stylesheet" href="style.css">
+
+    <!-- link to css -->
+ 
+    <!--  -->
+
+<body>
+    <div class="container">
+
+  <!-- header area -->
+    <header>
+        <img class="profile" src="pic5.jpg" alt="">
+        <div class="bio">
+            <h2>Rezwan Sheikh</h2>
+            <h2>Asst.Officer(MIS)</h2>
+        </div>
+        <!--  -->
+   
+        <!-- Contact area -->
+        <div class="contact">
+
+            <p><a href="tel:+8801960634996,01751998572">Phone : +8801960634996, 01751998572 </a></p>
+            <p><a href="mailto:sheikhrezwan9@gmail.com">Email : sheikhrezwan9@gmail.com</a></p>
+            <p><a href="https://github.com/RezwanMIS">Github : https://github.com/RezwanMIS</a></p>
+            <p><a href="https://www.linkedin.com/in/rezwan-sheikh-4b8361219">Linkedin : https://www.linkedin.com/in/rezwan-sheikh-4b8361219</a></p>
+            <p><a href="https://www.facebook.com/sk.rezwanulislam.1">Facebook : https://www.facebook.com/sk.rezwanulislam.1</a></p>
+
+        </div>
+        <!--  -->
+
+    </header>
+
+    <!-- div clear area -->
+     <div class="div-clear">
+     </div>
+     <!--  -->
+
+     <!-- Horrizontal rule area -->
+    <hr>
+    <!--  -->
+
+    <!-- main area -->
+    <main>
+
+    <!-- personal section area -->
+        <section id="personal-section">
+            <div class="content">
+                <div class="left-div">
+                    <h3>Personal Information</h3>
+
+                </div>
+                <div class="right-div">
+                <pre>
+                Father's Name: Amanat Sheikh
+                Mother's Name: Manowara Begum 
+                Gender: Male 
+                Date of Birth: May 16, 1992 
+                Height: 5.11"
+                Weight: 82(Kg)  
+                Nationality: Bangladeshi 
+                National Id No: 4643904537 
+                Permanent Address: Poranpur, Ratoil, Kashiani, Gopalganj 8132
+                </pre>
+               </div>
+
+            </div>
+
+        </section>
+
+        <div class="div-clear">
+        </div>
+        <hr class="common-hr">
+
+
+         <!-- workin experience -->
+         <section id="working-section">
+            <div class="content">
+                <div class="left-div">
+                    <h3>Working Experience</h3>
+
+                </div>
+                <div class="right-div">
+               <pre>
+               <b class="ref">Experience ( 2.3 Years)</b>
+               Assistant Officer Management Information System (MIS)
+               Joining date: (23 January, 2022 - Continuing)
+               Company: M.M . Knitwear Ltd.
+               Location: Konabari, Gazipur.
+               </pre>
+                <pre>
+               <b class="ref">Duties/Responsibilities:</b>
+               <b>(1).Daily Duties:</b> Basically I am working ERP software system technical data input 
+               support & problem solving all the merchandising & procurement & commercial team,
+               Create new users id in ERP module, provide ERP related training & monitor
+               the preparation of user manuals intended as a reference for end users.
+
+               <b>(2).Major Responsibilities:</b> Playing role of mid-level programming 
+               (i) Coding, (ii) Understanding the Requirements,
+               (iii) Designing the Software Architecture,
+               (iv) Designing the database, (v) Preparing Documentation.
+
+               <b>(3).Development Platform:</b> (i) HTML5, (ii) CSS3, (iii) JavaScript,
+               (iv) PHP, (v) MySQL Server , (vi) Visual Studio Code,
+               (vii) Chat GPT-3.5 & Open AI, (viii) VNC Viewer. 
+
+               <b>(4).My achievement:</b> I support a large team of 85 members with ERP 
+               software related support on a daily basis.
+               </pre>
+
+               </div>
+            </div>
+        </section>
+      <div class="div-clear">
+        </div>
+        <hr class="common-hr">
+
+
+         <!-- Training Summary section area -->
+         <section id="Training Summary">
+            <div class="content">
+                <div class="left-div">
+                    <h3>Training Summary</h3>
+
+                </div>
+                <div class="right-div">
+                <pre>
+                <b class="ref">Training Title: Computer Basic Course</b>
+                <b>Institute:</b> Govt. Technical Youth Development Training Institute 
+                <b>Duration:</b> 6 Month
+                </pre>
+               </div>
+              </div>
+            </section>
+         <div class="div-clear">
+        </div>
+        <hr class="common-hr">
+
+
+         <!-- Educational Qualification section area -->
+         <section id="Educational Qualification">
+            <div class="content">
+                <div class="left-div">
+                    <h3>Educational Qualification</h3>
+
+                </div>
+                <div class="right-div">
+                <pre>
+                <b class="ref">(1).Master of Information & Communication Technology (MICT)</b>
+                <b>Institute:</b> National University 
+                <b>Pass Year:</b> 2019 
+                <b>Major:</b> Information and Communication Technology (ICT) 
+                <b>Result:</b> CGPA 3.25 (Out of 4)
+                <b>Duration:</b> 1 Year
+
+                <b class="ref">(2).Master of Business Administration (MBA)</b>
+                <b>Institute:</b> BGMEA University of Fashion & Technology (BUFT) 
+                <b>Pass Year:</b> 2020 
+                <b>Major:</b> Apparel Merchandising 
+                <b>Result:</b> CGPA 2.81 (Out of 4)
+                <b>Duration:</b> 2 Year
+               </pre>
+               </div>
+
+            </div>
+
+        </section>
+
+        <div class="div-clear">
+        </div>
+        <hr class="common-hr">
+
+
+         <!-- Reference section area -->
+         <section id="Reference">
+            <div class="content">
+                <div class="left-div">
+                    <h3>Reference</h3>
+
+                </div>
+                <div class="right-div">
+                <pre>
+                <b class="ref">(1).Md. Rasel Ferdoush, (Head of ERP Developer & MIS)</b>
+                <b>Relation:</b> Professional 
+                <b>Mobile No:</b> 01730023243
+                <b>E-Mail:</b> ferdoush@mmknitwear.com
+
+                <b class="ref">(2).Dr.Md. Kamrul Hasan, (Associat Professor and Head)</b> 
+                <b>Relation:</b> Academic 
+                <b>Mobile No:</b> 01712558325
+                <b>E-Mail:</b> kamrul.hasan@buft.edu.bd
+               </pre>
+               </div>
+           </div>
+           </section>
+       <div class="div-clear">
+        </div>
+        <hr class="common-hr">
+
+    </main>
+    <!--  -->
+    <div class="div-clear">
+  <!-- footer area -->
+   <footer class="bottom">
+  <p>Copyright &copy; Rezwan Sheikh (18 Mar 2024)</p>
+  </footer>
+  <!--  -->
+
+    </div>
+    
+    
+</body>
+</html>
